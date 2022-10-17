7aefb7ed71a2e66015307d4ea51a21c45c2918fd
Manuel Benini <manuelbenini1905@gmail.com>
2022-10-17 17:15:18 +0200

    Aggiunta chiamata per cercare una pziza ed una show che mostra il dettaglio della pizza selezionata

--- Controllers/API/PizzaController.cs
+++ Controllers/API/PizzaController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Models;
 
 namespace la_mia_pizzeria_crud_webapi.Controllers.API
 {
-    [Route("Guest/api/[controller]")]
-    //[Route("[Page]/api/[controller]")]
+    //[Route("api/[controller]/[Action]")]
+    [Route("api/[controller]/[Action]")]
     [ApiController]
     public class PizzaController : ControllerBase
     {
@@ -22,6 +23,20 @@ namespace la_mia_pizzeria_crud_webapi.Controllers.API
             List<Pizza> pizzas = db.Pizzas.ToList();
 
             return Ok(pizzas);
+        }
+        [HttpGet]
+        public IActionResult SearchPizzas(string userSearch)
+        {
+            List<Pizza> pizzas = db.Pizzas.Where(p => p.Name.ToLower().Contains(userSearch.ToLower() )).ToList();
+
+            return Ok(pizzas);
+        }
+        [HttpGet]
+        public IActionResult Show(int id)
+        {
+            Pizza pizza = db.Pizzas.Where(p => p.PizzaId == id).FirstOrDefault();
+
+            return Ok(pizza);
         }
     }
 }
--- Controllers/GuestController.cs
+++ Controllers/GuestController.cs
@@ -9,8 +9,16 @@ namespace la_mia_pizzeria_crud_webapi.Controllers
 {
     public class GuestController : Controller
     {
+        PizzaContext db;
+
+        public GuestController()
+        {
+            db = new();
+        }
+
         public IActionResult Index() { return View(); }
         public IActionResult PizzasList() { return View(); }
+        public IActionResult Show() { return View(); }
 
     }
 }
